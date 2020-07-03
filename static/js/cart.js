fb192062a48f3d6cc22e2f3f0beadcd54336d972
Omkar Yadav <omkar.alternative@gmail.com>
2020-07-03 21:03:04 +0530

    noReload

--- static/js/cart.js
+++ static/js/cart.js
@@ -4,8 +4,10 @@ for (i = 0; i < updateBtns.length; i++){
 	updateBtns[i].addEventListener('click', function(){
 		var productId = this.dataset.product
 		var action = this.dataset.action
-		console.log('productId: ', productId, 'Action: ', action)
-
+		var price = this.dataset.price
+		console.log('productId: ', productId, 'Action: ', action, 'Price: ', price)
+		updateCartItems(productId, action, price)
+		// console.log(location.pathname)
 		console.log('USER: ', user)
 		if (user == 'AnonymousUser'){
 			addCookieItem(productId, action)
@@ -16,12 +18,12 @@ for (i = 0; i < updateBtns.length; i++){
 	})
 }
 
-function updateUserOrder(productId, action){
+async function updateUserOrder(productId, action){
 	console.log('User is authenticated, sending data...')
 
 		var url = '/update_item/'
 
-		fetch(url, {
+		await fetch(url, {
 			method:'POST',
 			headers: {
 				'Content-Type': 'application/json',
@@ -34,11 +36,63 @@ function updateUserOrder(productId, action){
 		})
 		.then((data) => {
 			console.log('Data: ', data)
-			location.reload()
+			// location.reload()
 		});
 }
 
-function addCookieItem(productId, action){
+async function updateCartItems(productId, action, price){
+	let total = document.getElementById('cart-total')
+	let cartSubtotalItems = document.getElementById('cart-subtotal-items')
+	let cartSubtotal = document.getElementById('cart-subtotal')
+	let prodQuan = document.querySelectorAll('#prod-quantity')
+
+	if (action == 'delete'){
+		location.reload()
+	}
+	prodQuan.forEach(item => {
+		if (productId == item.attributes.value.textContent) {
+			if (action == 'add'){
+				console.log(parseInt(item.textContent)+1)
+				item.textContent = parseInt(item.textContent)+1
+				// prodQuan.textContent = parseInt(prodQuan.textContent)+1
+			}
+			if (action == 'remove'){
+				console.log(parseInt(item.textContent)-1)
+				item.textContent = parseInt(item.textContent)-1
+				if (item.textContent==0){
+					location.reload()
+				}
+			}
+		}
+	})
+	// console.log(prodQuan[0])
+	// total.textContent =
+	console.log(productId)
+
+	if (action == 'add'){
+		// console.log(parseInt(total.textContent)+1)
+		total.textContent = parseInt(total.textContent)+1
+		if (cartSubtotal != null || cartSubtotal != undefined){
+			cartSubtotal.textContent = parseFloat(cartSubtotal.textContent)+parseFloat(price)
+		}
+		if (cartSubtotalItems != null || cartSubtotalItems != undefined){
+			cartSubtotalItems.textContent = parseInt(cartSubtotalItems.textContent)+1
+		}
+		// prodQuan.textContent = parseInt(prodQuan.textContent)+1
+	}
+	if (action == 'remove'){
+		// console.log(parseInt(total.textContent)-1)
+		total.textContent = parseInt(total.textContent)-1
+		if (cartSubtotalItems != null || cartSubtotalItems != undefined){
+			cartSubtotalItems.textContent = parseInt(cartSubtotalItems.textContent)-1
+		}
+		if (cartSubtotal != null || cartSubtotal != undefined){
+			cartSubtotal.textContent = parseFloat(cartSubtotal.textContent)-parseFloat(price)
+		}
+	}
+}
+
+async function addCookieItem(productId, action){
 	console.log('User is not authenticated')
 
 	if (action == 'add'){
@@ -66,5 +120,5 @@ function addCookieItem(productId, action){
 	console.log('CART:', cart)
 	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
 
-	location.reload()
+	// location.reload()
 }
