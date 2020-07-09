24318e6402837b788646d901bce6002637340abd
Omkar Yadav <omkar.alternative@gmail.com>
2020-07-09 22:19:52 +0530

    v1.0.1.1

--- store/models.py
+++ store/models.py
@@ -22,19 +22,19 @@ class Product(models.Model):
 	description 	= models.CharField(max_length=200, null=True)
 	price 			= models.DecimalField(max_digits=7, decimal_places=2)
 	availability 	= models.BooleanField(default=True)
-	image			= models.ImageField(null=True, blank=True)
+	image			= models.ImageField(default='/images/comingsoon.jpg',null=True, blank=True)
 
 	def __str__(self):
 		return self.name
 
-	@property
-	def imageURL(self):
-		try:
-			url 	= self.image.url
-		except:
-			url 	= '/images/comingsoon.jpg'
+	# @property
+	# def imageURL(self):
+	# 	try:
+	# 		url 	= self.image.url
+	# 	except:
+	# 		url 	= '/images/comingsoon.jpg'
 
-		return url
+	# 	return url
 
 class Order(models.Model):
 
