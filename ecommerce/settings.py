1acceae469434076c286c10d91b6504dcf1d7fd0
omkar <omkar.alternative@gmail.com>
2020-07-10 23:49:22 +0530

    s3 first try

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -161,10 +161,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 #Amazon S3
 
-# AWS_ACCESS_KEY_ID = 'AKIAV7V62O3GUDB52XEC'
-# AWS_SECRET_ACCESS_KEY = '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'
-# AWS_STORAGE_BUCKET_NAME = 'cornerstonecupcakes-bucket'
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL = None
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = 'AKIAV7V62O3GUDB52XEC'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'cornerstonecupcakes-bucket'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
