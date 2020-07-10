56f43bc18f3c0ae94fc8b2251388294c5c82e4eb
omkar <omkar.alternative@gmail.com>
2020-07-11 01:01:32 +0530

    s3 try 6

--- store/models.py
+++ store/models.py
@@ -22,7 +22,7 @@ class Product(models.Model):
 	description 	= models.CharField(max_length=200, null=True)
 	price 			= models.DecimalField(max_digits=7, decimal_places=2)
 	availability 	= models.BooleanField(default=True)
-	image			= models.ImageField(null=True, blank=True)
+	image			= models.ImageField(upload_to='images/' ,null=True, blank=True)
 
 	def __str__(self):
 		return self.name
@@ -32,7 +32,7 @@ class Product(models.Model):
 		try:
 			url 	= self.image.url
 		except:
-			url 	= 'images/comingsoon.jpg'
+			url 	= '/images/comingsoon.jpg'
 
 		return url
 
