d813b19a1cb550896f5315e4eb55cab78ef68c5f
Omkar Yadav <omkar.alternative@gmail.com>
2020-07-11 02:21:37 +0530

    product stock

--- store/models.py
+++ store/models.py
@@ -23,6 +23,7 @@ class Product(models.Model):
 	price 			= models.DecimalField(max_digits=7, decimal_places=2)
 	availability 	= models.BooleanField(default=True)
 	image			= models.ImageField(upload_to='images/' ,null=True, blank=True)
+	stock			= models.IntegerField(default=12, null=True)
 
 	def __str__(self):
 		return self.name
